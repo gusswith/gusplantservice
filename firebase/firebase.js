2828b5eda7a0aecfe05de46fcb84b7e016320fe6
gusswith <account@sorawith.com>
2026-01-17 17:39:08 +0700

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,10 +2,7 @@ var firebaseAdmin = require("firebase-admin");
 
 var serviceAccount = require("./gusplant-c2bdd-firebase-adminsdk-fbsvc-29768b2ed3.json");
 
-firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
-    databaseURL: "https://gusplant-c2bdd-default-rtdb.asia-southeast1.firebasedatabase.app"
-});
+firebaseAdmin.initializeApp();
 
 const firebaseDB = firebaseAdmin.database();
 module.exports = firebaseDB;
